0817e44af51f5a2536e7829de239253fbde4b5dc
Sina Moradian <sinacsharp@gmail.com>
2022-05-04 10:10:52 +0430

    Add click to the keys

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 window.addEventListener("keydown", e => {
-    const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
+    playAndStyle(e.keyCode);
+});
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => {
+    key.addEventListener('click', e => {
+        playAndStyle(e.target.getAttribute('data-key'));
+    });
+    key.childNodes.forEach(e => {
+        e.addEventListener('click', e => {
+            playAndStyle(e.target.parentElement.getAttribute('data-key'));
+        });
+    });
+});
+
+function playAndStyle(dataKey) {
+    const audio = document.querySelector(`audio[data-key='${dataKey}']`);
+    const key = document.querySelector(`.key[data-key='${dataKey}']`);
     if (!audio) return;
     audio.currentTime = 0;
     audio.play();
-});
+    key.classList.add('playing');
+}
